3b9ea1a4249e5ad4563be18aadca5abe41653ad6
korea8378 <korea8378@naver.com>
2018-10-07 18:36:58 +0900

    fist commit

---
+++ src/main/java/mashup/spring/study/pratice/PraticeApplication.java
@@ -0,0 +1,12 @@
+package mashup.spring.study.pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraticeApplication.class, args);
+    }
+}
