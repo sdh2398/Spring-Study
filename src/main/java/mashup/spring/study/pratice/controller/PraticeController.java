1b397c1fdcde48e7a2f62e8858934b8c4ad35d33
korea8378 <korea8378@naver.com>
2018-10-13 13:25:19 +0900

    test

--- src/main/java/mashup/spring/study/pratice/controller/PraticeController.java
+++ src/main/java/mashup/spring/study/pratice/controller/PraticeController.java
@@ -8,6 +8,6 @@ public class PraticeController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot!!!";
     }
 }
